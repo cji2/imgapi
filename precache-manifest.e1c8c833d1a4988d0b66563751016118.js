52e7b70756869f971759260c85059af10acfd25a
Changhoon Ji <cji2@gmu.edu>
2019-09-21 22:17:45 -0400

    Updates

---
+++ precache-manifest.e1c8c833d1a4988d0b66563751016118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26990580a2e367d3632e8c5713f74606",
+    "url": "/imgapi/index.html"
+  },
+  {
+    "revision": "faec15c109727f49fe06",
+    "url": "/imgapi/static/css/main.fbb14f17.chunk.css"
+  },
+  {
+    "revision": "bfcf9516b91892522f9e",
+    "url": "/imgapi/static/js/2.8e15f14f.chunk.js"
+  },
+  {
+    "revision": "faec15c109727f49fe06",
+    "url": "/imgapi/static/js/main.bafa7cf7.chunk.js"
+  },
+  {
+    "revision": "92b7f485db9376517555",
+    "url": "/imgapi/static/js/runtime~main.eb93b97f.js"
+  }
+]);
